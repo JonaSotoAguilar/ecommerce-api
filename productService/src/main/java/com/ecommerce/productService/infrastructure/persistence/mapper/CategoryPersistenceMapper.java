95f94f56948f62485f6fa95818c48991f417006d
Jonathan Soto Aguilar <jonasotoaguilar@gmail.com>
2025-08-19 13:01:46 -0400

    fix: mappers_fix

--- productService/src/main/java/com/ecommerce/productService/infrastructure/persistence/mapper/CategoryPersistenceMapper.java
+++ productService/src/main/java/com/ecommerce/productService/infrastructure/persistence/mapper/CategoryPersistenceMapper.java
@@ -3,10 +3,13 @@ package com.ecommerce.productService.infrastructure.persistence.mapper;
 import com.ecommerce.productService.domain.model.Category;
 import com.ecommerce.productService.infrastructure.persistence.entity.CategoryEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CategoryPersistenceMapper {
-    Category toDomain(CategoryEntity e);
+    Category toDomain(CategoryEntity entity);
 
-    CategoryEntity toEntity(Category d);
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    CategoryEntity toEntity(Category domain);
 }
